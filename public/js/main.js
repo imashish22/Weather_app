5897f0df58a792533162304579cdf2830f2a784e
Ashish <110475396+imashish22@users.noreply.github.com>
2023-09-23 16:12:50 +0530

    weather-app

---
+++ public/js/main.js
@@ -0,0 +1,39 @@
+const cityName  = document.getElementById('cityName')
+const submitBtn = document.getElementById('submitBtn');
+const city_name = document.getElementById('city_name')
+const temp_real_val =document.getElementById('temp_real_val')
+const temp_status =document.getElementById('temp_status')
+
+const getInfo = async (event) =>{
+    event.preventDefault()
+    let cityVal = cityName.value;
+    if(cityVal === ''){
+        city_name.innerText = `please write the name of the city`;
+    }else{
+        try {
+            let url = ` http://api.openweathermap.org/data/2.5/weather?q=${cityVal}&units=metric&appid=792e4604e771e2546e86458e74909d47` 
+            const response =await fetch(url);
+            const data =await response.json()
+            const arrData = [data]
+            city_name.innerText = `${arrData[0].name}. ${arrData[0].sys.country}`
+            temp_real_val.innerText = arrData[0].main.temp
+            const tempMood = arrData[0].weather[0].main
+
+            if(tempMood === 'Clear'){
+                temp_status.innerHTML =" <i class='fas fa-sun' style='color:#eccc68;' ></i>"
+            }else if(tempMood === 'Clouds'){
+                temp_status.innerHTML =" <i class='fas fa-cloud' style='color:#f1f2f6;' ></i>"
+            }else if(tempMood === 'Rain'){
+                temp_status.innerHTML =" <i class='fas fa-rain' style='color:#a4b0be;' ></i>"
+            }else{
+                temp_status.innerHTML =" <i class='fas fa-sun' style='color:#eccc68;' ></i>"
+            }
+
+        } catch (error) {
+            city_name.innerText = `please enter the name properly`;
+            
+        }
+       
+    }
+}
+submitBtn.addEventListener('click',getInfo)
--- src/app.js
+++ src/app.js
@@ -1,26 +1,39 @@
 const express = require("express")
 const path = require('path')
 const app = express()
+const hbs = require('hbs')
+
+
+
 const port = process.env.PORT || 8000
 
 const static_path =path.join(__dirname,"../public")
+const template_path =path.join(__dirname,"../templates/views")
+const partials_path =path.join(__dirname,"../templates/partials")
+
+app.set('view engine','hbs')
+app.set('views',template_path)
+hbs.registerPartials(partials_path)
+
 app.use(express.static(static_path))
 
 //routing
 app.get("/",(req,res)=>{
-    res.send("welcome to home channel")
+    res.render("index")
 })
 
 app.get("/about",(req,res)=>{
-    res.send("welcome to about channel")
+    res.render('about')
 })
 
 app.get("/weather",(req,res)=>{
-    res.send("welcome to weather channel ")
+    res.render("weather")
 })
 
 app.get("/*",(req,res)=>{
-    res.send("404 page error oopsssss ")
+    res.render('404error',{
+        errorMsg:'Opps! Page Not Found'
+    })
 })
 
 
